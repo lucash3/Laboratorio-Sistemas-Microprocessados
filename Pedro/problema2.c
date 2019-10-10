b2b7daadca2d5a650f0472d45124404a89f48606
Pedro Carvalho <43827142+pedrovrc@users.noreply.github.com>
2019-10-09 23:38:33 -0300

    update problema 2

--- Pedro/problema2.c
+++ Pedro/problema2.c
@@ -16,13 +16,25 @@
     OUT     ---->   Saida do sinal de frequencia gerado
 */
 
-// *** AINDA PRECISA ADICIONAR CONFIGURACOES DE PINO ***
-
 void main() {
-    unsigned char counter = 0;
-    int n, x, y, diff;
-    int R, G, B;
+    unsigned char seletor = 0;
+    unsigned int select_cor, medida1, medida2, diff;
+    unsigned int R, G, B;
 
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    PM5CTL0 &= ~(LOCKLPM5);     // destrava pinos digitais
+
+    // CONFIGURACOES DE PINO ---------------------------------------------------
+    P6DIR |= (BIT0|BIT1|BIT2|BIT3);     // habilita saída nos pinos
+    P6REN &= ~(BIT0|BIT1|BIT2|BIT3);    // desabilita resistor
+
+    P1DIR &= ~BIT6;     // habilita entrada no pino
+    P1SEL |= BIT6;      // habilita funcionalidade dedicada
+
+    P1DIR |= BIT0;      // habilita saida no pino (LED vermelho)
+    P6DIR |= BIT6;      // habilita saida no pino (LED verde)
+
+    // INICIO ------------------------------------------------------------------
     P6OUT |= BIT0;      // seleciona escala do sinal
     P6OUT &= ~BIT1;     // em 20% da frequencia original
                         // S0 = 1; S1 = 0
@@ -31,20 +43,20 @@ void main() {
     while (1) {
 
         // SELECAO DO MODO DE LEITURA ------------------------------------------
-        // com base no counter
-        switch (counter) {
+        // com base no seletor
+        switch (seletor) {
             case 0:
                 P6OUT &= ~(BIT2|BIT3);  // seleciona p/ luz vermelha
                 break;                  // S2 = 0; S3 = 0
 
             case 1:
+                P6OUT |= (BIT2|BIT3);   // seleciona p/ luz verde
+                break;                  // S2 = 1; S3 = 1
+
+            case 2:
                 P6OUT |= BIT3;          // seleciona p/ luz azul
                 P6OUT &= ~BIT2;         // S2 = 0; S3 = 1
                 break;
-
-            case 3:
-                P6OUT |= (BIT2|BIT3);   // seleciona p/ luz verde
-                break;                  // S2 = 1; S3 = 1
 
             default: break;
         }
@@ -52,63 +64,61 @@ void main() {
         // LEITURA DO SINAL RECEBIDO -------------------------------------------
         while (!(TB0CCTL1 & CCIFG));    // espera flanco do sinal recebido
         TB0CCTL1 &= ~CCIFG;
-        x = TB0CCR1;                    // salva contagem de tempo do sinal
-                                        // repete para variavel y
+        medida1 = TB0CCR1;              // salva contagem de tempo do sinal
+                                        // repete medicao
         while(!(TB0CCTL1 & CCIFG));
         TB0CCTL1 &= ~CCIFG;
-        y = TB0CCR1;
+        medida2 = TB0CCR1;
 
-        diff = y - x;                   // diff = periodo do sinal recebido
+        diff = medida2 - medida1;       // diff = periodo do sinal recebido
                                         // periodo indica intensidade luminosa
                                         // maior -> luz mais intensa
                                         // menor -> luz mais fraca
 
-
         // PASSAGEM DE CONTADOR PARA OUTRAS LEITURAS --------------------------
         // e atribuicao de valor lido p/ variaveis de cor
-        switch (counter) {
+        switch (seletor) {
             case 0:
                 R = diff;               // intensidade vermelha salva
-                counter++;              // counter: 0 -> 1
-                break;                  // prox leitura: azul
+                seletor++;              // seletor: 0 -> 1
+                break;                  // prox leitura: verde
 
             case 1:
-                B = diff;               // intensidade azul salva
-                counter++;              // counter: 1 -> 3
-                counter++;              // prox leitura: verde
-                break;
-
-            case 3:
                 G = diff;               // intensidade verde salva
-                counter = 0;            // counter: 3 -> 0
-                                        // prox leitura: vermelho (reset)
+                seletor++;              // seletor: 1 -> 2
+                break;                  // prox leitura: azul
+
+            case 2:
+                B = diff;               // intensidade azul salva
+                seletor = 0;            // seletor: 2 -> 0
+                                        // prox leitura: vermelha (reset)
                 flag = 1;               // seta flag p/ fazer comparacao
                 break;                  // das intensidades luminosas
         }
-
 
         // INTERPRETACAO DAS LEITURAS ------------------------------------------
         // flag so e setada quando as leituras
         // R, G e B ja foram executadas
         if (flag == 1) {
-            TB0CTL = MC_1     |         // reseta contador
-                     TASSEL.1 |
-                     TACLR;
+            /*
+            TB0CTL = MC_1     |
+                     TBSSEL_1 |
+                     TBCLR;
 
             TB0CCTL1 = CM_1 |
                        SCS  |
                        CAP;
-
+            */
             // algoritmo de comparacao de maior numero
             maior = vermelho;
-            n = 0;
+            select_cor = 0;
             if (maior < verde) {
                 maior = verde;
-                n = 1;
+                select_cor = 1;
             }
             if (maior < azul) {
                 maior = azul;
-                n = 2;
+                select_cor = 2;
             }
 
             // FEEDBACK DA LEITURA ---------------------------------------------
@@ -121,7 +131,7 @@ void main() {
             // Verde            ----    R = 0; G = 1
             // Azul             ----    R = 1; G = 1
 
-            switch (n) {
+            switch (select_cor) {
                 case 0:                 // caso vermelho dominante
                     P1OUT |= BIT0;      // R = 1
                     P6OUT &= ~BIT6;     // G = 0

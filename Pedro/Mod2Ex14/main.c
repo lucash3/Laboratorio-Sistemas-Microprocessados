c3564894bda3e2209cf30699f90102d4315a4d92
Pedro Carvalho <43827142+pedrovrc@users.noreply.github.com>
2019-10-16 19:58:43 -0300

    Ex14 completo

--- Pedro/Mod2Ex14/main.c
+++ Pedro/Mod2Ex14/main.c
@@ -1,6 +1,11 @@
 #include <msp430.h>
 #include <stdint.h>
 
+void debounce (volatile uint16_t dt) {
+    while(dt--);
+    return;
+}
+
 #pragma vector = TIMER0_B0_VECTOR
 __interrupt void TB0_CCR0_ISR() {
     P1OUT ^= BIT0;
@@ -13,8 +18,6 @@ __interrupt void TB0_CCR1_ISR() {
 }
 
 int main(void) {
-    unsigned int duty_cycle = 50;
-
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
     PM5CTL0 &= ~(LOCKLPM5);     // destrava pinos digitais
 
@@ -25,15 +28,15 @@ int main(void) {
     // Config. S2
     P2DIR &= ~BIT3;
     P2REN |= BIT3;
-    P2OUT &= ~ BIT3;
+    P2OUT |= BIT3;
 
     // Config. S1
     P4DIR &= ~BIT1;
     P4REN |= BIT1;
-    P4OUT &= ~BIT1;
+    P4OUT |= BIT1;
 
     // Config. timer
-    TB0CTL = (TBSSEL__SMCLK |   // seleciona SMCLK @1048576Hz
+    TB0CTL = (TBSSEL__ACLK  |   // seleciona ACLK @32768Hz
               MC__UP        |   // modo de contagem UP
               TBCLR);           // comecando do zero
 
@@ -42,23 +45,23 @@ int main(void) {
 
     __enable_interrupt();
 
-    TB0CCR0 = (1048576/50) - 1;
-    TB0CCR1 = ((1048576/50) * (duty_cycle/100)) - 1;
+    TB0CCR0 = 655 - 1;          // 655 = 32768/50
+    TB0CCR1 = 327 - 1;          // 327 = 655/2
 
     while (1) {
-        if (!(P4IN & BIT1) || !(P2IN & BIT3)) {     // se algum botao for press.
-            if (!(P4IN & BIT1) && (P2IN & BIT3)) {  // S1 press. S2 solto
-                // decrementa duty cycle
-                if (duty_cycle == 0);
-                else duty_cycle -= 10;
-            }
-            if (!(P2IN & BIT3) && (P4IN & BIT1)) {  // S2 press. S1 solto
-                // incrementa duty cycle
-                if (duty_cycle == 100);
-                else duty_cycle += 10;
-            }
-            TB0CCR1 = ((1048576/50) * (duty_cycle/100)) - 1;
+        while ((P4IN & BIT1) && (P2IN & BIT3));     // espera botao ser press.
+        debounce(5000);
+        if ((P4IN & BIT1) && !(P2IN & BIT3)) {  // S1 press. S2 solto
+            // decrementa duty cycle
+            TB0CCR1 -= 65;      // 65 = 10% duty cycle
         }
+        if ((P2IN & BIT3) && !(P4IN & BIT1)) {  // S2 press. S1 solto
+            // incrementa duty cycle
+            TB0CCR1 += 65;
+        }
+
+        while (!(P4IN & BIT1) || !(P2IN & BIT3));     // espera botao ser solto
+        debounce(5000);
     }
 
     return 0;

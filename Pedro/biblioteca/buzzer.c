1f559fc0c168cd8a0a193343682559cfad2477cd
Pedro Carvalho <43827142+pedrovrc@users.noreply.github.com>
2019-11-28 08:32:09 -0300

    Add files via upload

--- Pedro/biblioteca/buzzer.c
+++ Pedro/biblioteca/buzzer.c
@@ -11,3 +11,37 @@
 #define notaA3s 464
 #define notaB3 490
 #define notaC4 516
+#define notaC4s 553
+#define notaD4 584
+#define notaD4s 621
+#define notaE4 656
+#define notaF4
+#define notaF4s
+#define notaG4
+#define notaG4s
+#define notaA4
+#define notaA4s
+#define notaB4
+
+/*
+ *     int estrelinha[14];
+    [notaC3, notaC3, notaG3, notaG3, notaA3, notaA3, notaG3,
+     notaF3, notaF3, notaE3, notaE3, notaD3, notaD3, notaC3];
+
+    int i = 0;
+    while (i < 14) {
+        if (i < 2 || i == 13)
+            estrelinha[i] = notaC3;
+        else if (i < 4 || i == 6)
+            estrelinha[i] = notaG3;
+        else if (i < 6)
+            estrelinha[i] = notaA3;
+        else if (i < 9)
+            estrelinha[i] = notaF3;
+        else if (i < 11)
+            estrelinha[i] = notaE3;
+        else
+            estrelinha[i] = notaD3;
+        i++;
+    }
+ */

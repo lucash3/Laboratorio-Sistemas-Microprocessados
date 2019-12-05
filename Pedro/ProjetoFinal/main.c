ec30241effd67a65986418c8100bbda2168597f7
Pedro Carvalho <43827142+pedrovrc@users.noreply.github.com>
2019-12-04 23:49:29 -0300

    Add files via upload

--- Pedro/ProjetoFinal/main.c
+++ Pedro/ProjetoFinal/main.c
@@ -1,59 +1,57 @@
 #include <msp430.h> 
-//#include "../biblioteca/LCD_base.c"
 #include "../biblioteca/gpio.c"
 #include "../biblioteca/clock.c"
 #include "../biblioteca/buzzer.c"
+#include "../biblioteca/songs.c"
+#include "../biblioteca/I2C/lcd.c"
 
-volatile int durationFG = 0;
-
-// nota em Hz, duracao em segundos
-void play(int note, float duration) {
-
-    TB0CCR0 = get_N(note, FREQUENCY, MY_SMCLK);
-    if (note == 0) {
-        TB0CCR0 = 0;
-        P5OUT &= ~BIT4;
-    } else {
-        P5OUT |= BIT4;
-    }
-    TB0CTL |= TBSSEL__SMCLK;
-
-    TB1CCR0 = get_N(duration, PERIOD, MY_ACLK);
-    durationFG = 0;
-    TB1CTL |= TBSSEL__ACLK;
-
-    while(durationFG == 0);
-
-    return;
-}
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-    UNLOCK_MASTER_PIN;
+    WDTCTL = WDTPW | WDTHOLD;       // stop watchdog timer
+    UNLOCK_MASTER_PIN;              // Destrava pinos digitais
 
     config_all_buttons();
     config_all_leds();
 
-    _Bool fechado = false;          // Aberto por padrão
+    i2cInit(1, 0);
+    lcdInit();
+
+    /*
+     * Configurações para uso da função de botão da biblioteca gpio
+     * fechado é o estado atual do botão
+     * memória é o estado anterior
+     *
+     */
+
+    _Bool fechado = false;
     _Bool memoria = fechado;
 
-    TB0CTL  = TBSSEL__SMCLK |        // Configura SMCLK @1048576 Hz
-                  MC__UP       |        // Modo de contagem UP
-                  TBIE         |        // Habilita interrupção de overflow
-                  TBCLR;                // Começa do zero
+    /*
+     * TB0 usado para gerar a PWM que gera a nota musical
+     * TB1 usado para tocar a nota por um periodo de tempo determinado
+     * */
 
-    TB1CTL  = TBSSEL__ACLK |        // Configura ACLK @32768 Hz
-                      MC__UP       |        // Modo de contagem UP
-                      TBIE         |        // Habilita interrupção de overflow
-                      TBCLR;                // Começa do zero
+    TB0CTL  = TBSSEL__SMCLK |
+              MC__UP        |
+              TBIE          |
+              TBCLR;
 
-    TB0CCTL1 = CCIE;                // Habilita interrupção localmente Do canal 1 (permite ver a flag ccifg)
+    TB1CTL  = TBSSEL__ACLK |
+              MC__UP       |
+              TBIE         |
+              TBCLR;
+
+    TB0CCTL1 = CCIE;
     TB1CCTL1 = CCIE;
 
-    P1DIR |= BIT1;              // PXDIR = 1 => saida
+    // Saída para o buzzer
+
+    P1DIR |= BIT1;
     P1REN &= ~BIT1;
     P1OUT &= ~BIT1;
+
+    // Saída para o LED externo
 
     P5DIR |= BIT4;
     P5REN &= ~BIT4;
@@ -61,48 +59,58 @@ int main(void)
 
     __enable_interrupt();           // Seta GIE no registro SR
 
+    // Índice de música na lista
+
+    int musica = 0;
+
     while(1)
     {
-        play(notaC3, 0.3);
-        play(0, 0.2);
-        play(notaC3, 0.3);
-        play(0, 0.2);
-        play(notaG3, 0.3);
-        play(0, 0.2);
-        play(notaG3, 0.3);
-        play(0, 0.2);
-        play(notaA3, 0.3);
-        play(0, 0.2);
-        play(notaA3, 0.3);
-        play(0, 0.2);
-        play(notaG3, 0.3);
-        play(notaG3, 0.3);
-        play(0, 0.2);
 
+        lcdClear();
+        switch(musica) {
+        case 0:
+            lcdPrint("Jingle Bells\n");
+            lcdPrint("200 bpm");
+            jingleBell();
+            break;
+        case 1:
+            lcdPrint("Happy Birthday\n");
+            lcdPrint("80 bpm");
+            happyBirthday();
+            break;
+        case 2:
+            lcdPrint("Little Star\n");
+            lcdPrint("120 bpm");
+            littleStar();
+            break;
+        }
+
+        // Checagem do botão pressionado
         update_button_state(S1ORS2, &fechado, &memoria);
         if (fechado == memoria) continue;
         else if (fechado) {
             if (button_pressed(S1)) {
-                TB0CTL  ^= TBSSEL__SMCLK;
+                if (musica > 0) musica--;
             }
             if (button_pressed(S2)) {
-                // proxima musica
+                if (musica < 2) musica++;
             }
         }
-        //debounce(5000);
+        debounce(5000);
     }
 
     return 0;
 }
 
 
+/*
+ * Interrupção usada para gerar a PWM da nota musical
+ * */
+
 #pragma vector = TIMER0_B1_VECTOR
-__interrupt void TB0_ISR()     //EXCLUSIVO CCR0
+__interrupt void TB0_ISR()
 {
     switch (TB0IV) {
-        case 0x2:              // Do canal 1
-            //P1OUT ^= BIT1;
-            break;
         case 0xE:              // Overflow
             P1OUT ^= BIT1;
             break;
@@ -111,12 +119,15 @@ __interrupt void TB0_ISR()     //EXCLUSIVO CCR0
     }
 }
 
+/*
+ * Interrupção para medir o tempo de duração da nota e interromper
+ * quando necessário
+ * */
+
 #pragma vector = TIMER1_B1_VECTOR
-__interrupt void TB1_ISR()     //EXCLUSIVO CCR0
+__interrupt void TB1_ISR()
 {
     switch (TB1IV) {
-        case 0x2:              // Do canal 1
-            break;
         case 0xE:              // Overflow
             durationFG = 1;
             TB0CTL &= ~TBSSEL__SMCLK;
